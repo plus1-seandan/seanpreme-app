23e59c35fed2cfda7b5f92e1f3455287bd3abc09
Sean Dan <seandan@seans-mbp.myfiosgateway.com>
2021-02-03 19:42:30 -0500

    implemented redux-persist

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./redux/store";
 import Routes from "./routes";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <ChakraProvider>
       <BrowserRouter>
         <React.StrictMode>
-          <Routes />
+          <PersistGate persistor={persistor}>
+            <Routes />
+          </PersistGate>
         </React.StrictMode>
       </BrowserRouter>
     </ChakraProvider>
